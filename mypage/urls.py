ba1821435fb87016982a2f127f4745217df5328b
JinjeongPark <dbelroper@gmail.com>
2022-06-20 13:12:14 +0000

    move homepage file 2 mypage

--- mypage/urls.py
+++ mypage/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from mypage import views
 from django.urls import path
 
+#urlpatterns=[
+ #       path('',views.index,name='index'),
+#        ]
+
 urlpatterns=[
         path('',views.index,name='index'),
         ]
--- mypage/views.py
+++ mypage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<h1>This is the music app web page.</h1>")
+#def index(request):
+ #   return HttpResponse("<h1>This page is for opensourceSW final assignment</h1>")
 # Create your views here.
+
+def index(request):
+    return render(request,'index2.html')
